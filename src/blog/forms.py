711fe8110794dfc37581ff7aa49bc4bcd7698fca
Shaxzod ) <69134199+ShaxaDev@users.noreply.github.com>
2021-07-25 12:46:07 +0500

    Update forms.py

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -1,16 +1,19 @@
 from django import forms
 
 from blog.models import BlogPost ,Category
+from account.models import Territory
 
 
 class CreateBlogPostForm(forms.ModelForm):
 	title=forms.CharField(label="<b>Maqola nomi</b>",max_length=100,widget=forms.TextInput(attrs={'placeholder':'Maqola nomini yozing'}))
 	category=forms.ModelChoiceField(label="<b>Kategoriya</b>",empty_label='Kategoriya tanlang',queryset=Category.objects.all())
+	category=forms.ModelChoiceField(label="<b>Xudud</b>",empty_label='Kategoriya tanlang',queryset=Territory.objects.all())
+	
 	body=forms.CharField(label='<b>Maqola matni</b>',widget=forms.Textarea())
 	image=forms.ImageField(label='<b>Maqola uchun rasm</b>')
 	class Meta:
 		model = BlogPost
-		fields = ['title', 'category','body', 'image']
+		fields = ['title', 'category','territory','body', 'image']
 
 
 class UpdateBlogPostForm(forms.ModelForm):
